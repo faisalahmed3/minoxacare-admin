6ad2db0c06895c44c45fb3d663cbd956f2c4ad1e
faisalahmed3 <faisalahmed4417@gmail.com>
2026-06-24 21:16:31 +0600

    Repo Created

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-base-200">
+      <button className="btn btn-primary">Tailwind + DaisyUI Working</button>
+    </div>
+  )
+}
